7def821690cc55d4c7bacc55c336888f0e60730c
lpe234 <lpe234@qq.com>
2017-07-19 16:15:13 +0800

    add editor

---
+++ src/views/ArticleEditor.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="article-editor">
+
+    <p v-if="$route.meta.action==='create'">
+      article-create
+    </p>
+    <p v-else>
+      article-edit: {{ $route.params.id }}
+    </p>
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'article-editor'
+  }
+</script>
+
+<style lang="less" scoped>
+
+  @import "../assets/basic";
+
+  .article-editor {
+
+  }
+</style>
